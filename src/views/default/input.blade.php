251f35563a5dc30bfd41d5411a47b086bc76cf49
Ivan Shumakov <wymakob@gmail.com>
2019-02-04 12:55:36 +0200

    temp bug

--- src/views/default/input.blade.php
+++ src/views/default/input.blade.php
@@ -1,7 +1,7 @@
 @if($params = new Vannyvann\Formgroup\FormgroupParams($params, $errors, $options, $config))
 
     @component('formgroup::default.template', compact('params'))
-        <input {!! $params->basicOptions(['class' => 'form-control', 'type' => 'text']) !!}>
+        <input {!! $params->basicOptions(['class' => 'form-control']) !!}>
     @endcomponent
 
 @endif
